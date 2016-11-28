225731d24be8ec6d7f5647663e4d2bf737904cdb
apollo <apollo1133880@gmail.com>
2016-11-29 04:42:52 +0900

    first commit

---
+++ st-ad.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ショートコードで読み込むアドセンス
+ */
+?>
+<p style="color:#666;margin-bottom:5px;">スポンサーリンク</p>
+	<div class="middle-ad">
+		<?php if ( st_is_mobile() ) { //スマートフォンの時は300pxサイズを ?>
+				<?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 4 ) ) : else : ?>
+				<?php endif; ?>
+				<?php
+			} else {  //PCの時は336pxサイズを
+				?>
+				<?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 3 ) ) : else : ?>
+				<?php endif; ?>
+				<?php
+		}?>
+	</div>
